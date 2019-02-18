1447fbac56d89fa902370dd2c5e2a235936e4d37
SebFlatBeat <sebastien.darre@gmail.com>
2019-02-18 20:58:39 +0100

    Deplacement des methodes, elles etaient implementées dans la mauvaise classe

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -5,49 +5,14 @@ public class Main {
 
     public static void main (String[] args){
         int numPlayer = 1;
-        Main player1 = creerPlayer(numPlayer); // Création du player 1
+        Personnages player1 = Personnages.creerPlayer(numPlayer); // Création du player 1
         System.out.println(player1.toString());
         numPlayer ++;
-        Main player2 = creerPlayer(numPlayer); // Creation du player 2
+        Personnages player2 = Personnages.creerPlayer(numPlayer); // Creation du player 2
         System.out.println(player2.toString());
         player1.setAdversaire(player2);
         player2.setAdversaire(player1);
-
-    }
-
-    private static Main creerPlayer (int numPlayer) {
-        Personnages P;
-        int persoName,niveau,force,agilite,intelligence;
-        System.out.println("Création du Player " + numPlayer);
-        persoName = demande ("Choisisez votre personnage : (1 : Guerrier, 2 : Rôdeur , 3 : Mage)",1 , 3);
-        do {
-            niveau = demande("Choisissez le niveau de vôtre personnage : ", 1,100);
-            force = demande("Maintenant la force de vôtre personnage : " ,0,100);
-            agilite = demande("Au tour de l'agilité de votre personnage :" , 0,100);
-            intelligence = demande("Et enfin, l'intelligence de vôtre personnage", 0 ,100);
-            if (force+agilite+intelligence != niveau)
-                System.out.println("Attention votre total de force + agilité + intelligence ne doit pas DÉPASSER votre niveau !!!!");
-        }while (force+agilite+intelligence != niveau);
-        switch (persoName) {
-            case 1 :
-               P = new Guerrier(numPlayer,force,agilite,intelligence);
-                break;
-            case 2 :
-                P = new Rodeur(numPlayer,force,agilite,intelligence);
-                break;
-            case 3 :
-                P = new Mage(numPlayer,force,agilite,intelligence);
-                break;
-            default:
-                P = new Mage(numPlayer,force,agilite,intelligence);
-        }
-
-        return P;
-    }
-
-    public static int demande (String string, int min, int max){
-
-        return;
+        player1.activePersonnage();
     }
 
 
--- src/Game/Personnages.java
+++ src/Game/Personnages.java
@@ -72,11 +72,40 @@ public abstract class Personnages extends Main {
         this.intelligence = intelligence;
     }
 
+    protected static Personnages creerPlayer (int numPlayer) {
+        Personnages P;
+        int persoName,niveau,force,agilite,intelligence;
+        System.out.println("Création du Player " + numPlayer);
+        persoName = demande ("Choisisez votre personnage : (1 : Guerrier, 2 : Rôdeur , 3 : Mage)",1 , 3);
+        do {
+            niveau = demande("Choisissez le niveau de vôtre personnage : ", 1,100);
+            force = demande("Maintenant la force de vôtre personnage : " ,0,100);
+            agilite = demande("Au tour de l'agilité de votre personnage :" , 0,100);
+            intelligence = demande("Et enfin, l'intelligence de vôtre personnage", 0 ,100);
+            if (force+agilite+intelligence != niveau)
+                System.out.println("Attention votre total de force + agilité + intelligence ne doit pas DÉPASSER votre niveau !!!!");
+        }while (force+agilite+intelligence != niveau);
+        switch (persoName) {
+            case 1 :
+                P = new Guerrier(numPlayer,force,agilite,intelligence);
+                break;
+            case 2 :
+                P = new Rodeur(numPlayer,force,agilite,intelligence);
+                break;
+            case 3 :
+                P = new Mage(numPlayer,force,agilite,intelligence);
+                break;
+            default:
+                P = new Mage(numPlayer,force,agilite,intelligence);
+        }
+        return P;
+    }
+
     public void activePersonnage() {
         int choice;
         if (vie > 0) {
             do {
-                choice = Main.demande((persoName + "(" + vie + " vitalité ), veuillez choisir votre attaque (1 : attaque basqiue, 2: attaque spéciale"), 1, 2);
+                choice = Personnages.demande((persoName + "(" + vie + " vitalité ), veuillez choisir votre attaque (1 : attaque basqiue, 2: attaque spéciale"), 1, 2);
             if (choice != 1 && choice != 2)
                 System.out.println("Vous n'avez par rentré le bon choix !");
         } while (choice != 1 && choice != 2);
@@ -87,6 +116,15 @@ public abstract class Personnages extends Main {
         System.out.println("Le " + persoName + " à perdu !");
     }
     }
+    public static int demande (String string, int min, int max){
+        int choice;
+        do {
+            choice = demande(string,min)
+        }while (choice>max);
+
+        return min;
+    }
+
     protected abstract String play (int choice);
 
     public void setAdversaire (Personnages adversaire) {
